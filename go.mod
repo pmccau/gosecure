8299402ce269966249305f2030c86c2a7bd454ae
Patrick McCauley <pmccau@seas.upenn.edu>
2020-08-28 18:25:14 -0400

    GPIO start

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gosecure
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/stianeikeland/go-rpio v4.2.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/stianeikeland/go-rpio"
 	"gosecure/router"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	err := rpio.Open()
+	if err != nil {
+		log.Fatal("Error opening pins", err)
+	}
+	garagePin := rpio.Pin(18)
+	garagePin.Input()
+	frontDoorPin := rpio.Pin(23)
+	frontDoorPin.Input()
+
+	for {
+		fmt.Println("Garage res:", garagePin.Read())
+		fmt.Println("Front res:", frontDoorPin.Read())
+	}
+
 	r := router.Router()
 	fmt.Println("Starting server on port 8080...")
 
 	port := ":8080"
-	err := http.ListenAndServe(port, r)
+	err = http.ListenAndServe(port, r)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
