a975361f667264403d14ac3043042faed0939970
Patrick McCauley <pmccau@seas.upenn.edu>
2020-08-31 09:28:07 -0400

    Update

--- main.go
+++ main.go
@@ -9,37 +9,6 @@ import (
 
 func main() {
 
-	//err := rpio.Open()
-	//if err != nil {
-	//	log.Fatal("Error opening pins", err)
-	//}
-	//garagePin := rpio.Pin(18)
-	//garagePin.Input()
-	//garagePin.Detect(rpio.RiseEdge)
-	//frontDoorPin := rpio.Pin(23)
-	//frontDoorPin.Input()
-	//frontDoorPin.Detect(rpio.RiseEdge)
-	//
-	//for {
-	//	if frontDoorPin.EdgeDetected() {
-	//		fmt.Println("Edge at front door!")
-	//	}
-	//}
-
-//	for {
-//		garage := garagePin.Read()
-//		frontDoor := frontDoorPin.Read()
-//		if frontDoor == 1 {
-//			fmt.Println("Exiting, front door hot!")
-//			break
-//		}
-//		if garage == 1 {
-//			fmt.Println()
-//		}
-////		fmt.Println("Garage res:", garagePin.Read())
-////		fmt.Println("Front res:", frontDoorPin.Read())
-//	}
-
 	r := router.Router()
 	fmt.Println("Starting server on port 8080...")
 
